b99e764e23f5dc8c1819dbb11765bf68675da2f8
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-01-13 21:56:36 -0500

    Added initial Experience component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Experience.module.css';
+
+export const Experience = () => {
+    return (
+        <>
+            <br/>
+            <br/>
+            <h2 style={{ color: 'red', textAlign: 'center', fontSize: '24px' }}>
+                THIS SITE IS UNDER CONSTRUCTION, PLEASE REACH OUT TO ME FOR MY RESUME
+            </h2>
+        </>
+    )
+}
