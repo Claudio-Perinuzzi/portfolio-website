b0552ae7fb7c8749974f7f28620dba7029698b79
Claudio Perinuzzi <claudioperinuzzi@Claudios-MacBook-Pro.local>
2025-01-13 21:52:37 -0500

    Added commit component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,52 @@ import styles from './About.module.css';
 
 export const About = () => {
     return (
-        <>In Progress!</>
+        <>
+            <section id="about">
+                
+                <h1 className={styles.aboutHeader}>About</h1>
+
+                <div className={styles.windowContainer}>
+                    <div className={styles.tabHeader}>
+                            about_me.py
+                    </div>
+                        <div className={styles.ideContainer}>
+                            <span className={styles.import}>from</span>{" "}
+                            <span className={styles.variable}>about</span>{" "}
+                            <span className={styles.import}>import</span>{" "}
+                            <span className={styles.function}>me</span> <br /> <br />
+                            <span className={styles.vaideContainerriable}>myself</span>{" "}
+                            <span className={styles.operator}>=</span>{" "}
+                            <span className={styles.function}>me</span>
+                            <span className={styles.parenthesis}> ()</span>
+                            <br />
+                            <span className={styles.function}>print</span>
+                            <span className={styles.parenthesis}> (</span>
+                            <span className={styles.variable}>myself</span>
+                            <span className={styles.parenthesis}>)</span>
+                        </div>
+                    
+                    <div className={styles.terminalHeader}>
+                            Terminal - zsh
+                    </div>
+
+                    <div className={styles.terminal}>
+                        last login: {new Date().toLocaleString()} on console <br /> 
+                        (base) cp ~ % python about_me.py
+                        <br/>
+                        <br/>
+                        <div className= {styles.aboutText}>
+                            Hi, I'm Claudio Perinuzzi! I'm pursuing a Master's in Computer Science 
+                            at Queens College, graduating in May 2025. I'm passionate about machine 
+                            learning and excited to create solutions for real-world challenges. In my 
+                            free time, I enjoy spending time in nature, hiking, and staying active. 
+                            Feel free to check my work out below!                        
+                        </div>
+                    </div>
+                
+                </div>    
+            </section>
+
+        </>
     )
 }
