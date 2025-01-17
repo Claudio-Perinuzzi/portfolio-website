e9a3db20317e7bc3afc60bbb06d9fab0906f1df9
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-01-16 21:37:21 -0500

    Added project data

---
+++ src/components/Project/projectData.js
@@ -0,0 +1,68 @@
+export const projectData = [
+    {
+        projHeader: 'Learn_American_Sign_Language.txt',
+        title: 'Learn-ASL',
+        time: 'December 2024',
+        imgPath: 'asl_demo.gif',
+        ghUrl: 'https://github.com/jaynopponep/Learn-ASL',
+        demoUrl: false,
+        description: [
+            { text: 'Collaborated with a team to train and fine-tune an Ultralytics YOLO object detection model for recognizing and classifying ASL (American Sign Language) gestures, ', highlight: false },
+            { text: 'achieving a precision of 97.8%.', highlight: true },
+            { text: '', break: true },
+            { text: '', break: true },
+            { text: 'Built the ', highlight: false },
+            { text: 'backend in Python, ', highlight: true },
+            { text: 'leveraged Roboflow for data preprocessing and augmentation, and contributed to developing a ', highlight: false },
+            { text: 'React-based frontend in JavaScript. ', highlight: true }
+        ]
+    },
+    {
+        projHeader: 'NYC_AQI_Predictor.txt',
+        title: 'NYC AQI Predictor',
+        time: 'September 2024',
+        imgPath: 'aqi_img.png',
+        ghUrl: 'https://github.com/Claudio-Perinuzzi/nyc-air-quality-heat-map-predictor',
+        demoUrl: "https://nyc-aqi-predictor.streamlit.app/",
+        description: [
+            { text: 'Developed a ', highlight: false },
+            { text: 'Python web app with Streamlit ', highlight: true },
+            { text: 'to visualize and predict AQI (Air Quality Index) for NYC neighborhoods.', highlight: false },
+            { text: '', break: true },
+            { text: '', break: true },
+            { text: 'Utilized ', highlight: false },
+            { text: 'pandas for data manipulation, geopandas and folium for geospatial mapping, and scikit-learn for machine learning. ', highlight: true },
+            { text: 'The application trains a linear regression model to predict future AQI values.', highlight: false }   
+        ]
+    },
+    {
+        projHeader: 'Social_Media_Addiction_Predictor.txt',
+        title: 'Social Media Addiction Predictor',
+        time: 'May 2024',
+        imgPath: 'smup_img.png',
+        ghUrl: 'https://github.com/Claudio-Perinuzzi/social-media-usage-predictor',
+        demoUrl: "https://social-media-usage-predictor.streamlit.app/",
+        description: [
+            { text: "Implemented and trained a Random Forest machine learning model in Java ", highlight: true },
+            { text: " to predict social media addiction given user-provided information, including social media habits and socioeconomic background.", highlight: false },
+            { text: '', break: true },
+            { text: '', break: true },
+            { text: 'The model ', highlight: false },
+            { text: "achieves an average accuracy of 98%, ensuring reliable predictions.", highlight: true },                
+        ]
+    },
+    {
+        projHeader: 'PDF_Encrypt.txt',
+        title: 'PDF Encrypt',
+        time: 'July 2023',
+        imgPath: 'pdf_encrypt.png',
+        ghUrl: 'https://github.com/Claudio-Perinuzzi/encrypt-pdf',
+        demoUrl: false,
+        description: [
+            { text: "Developed a Python-based Graphical User Interface (GUI) to ", highlight: false },
+            { text: "efficiently encrypt multiple PDFs simultaneously, ", highlight: true },
+            { text: "allowing users to ", highlight: false },
+            { text: "save time by securely encrypting files in bulk.", highlight: true }
+        ]
+    }
+];
