1128f80cafead84dbc58422059eb87d3fdac39a5
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-01-16 20:00:47 -0500

    Added button images

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -5,25 +5,36 @@ export const ContactCard = () => {
     return (
         <section className={styles.contactContainer}>
             <img src="headshot_square.png" alt="Image of me" className={styles.contactImg}/>
+                
                 <div className={styles.textContainer}>
                     <div className={styles.nameContainer}>
                         <h1 className={styles.fname}>Claudio</h1>
                         <h1 className={styles.lname}>Perinuzzi</h1>
                     </div>
-                    <p className={styles.description}>I'm an Aspiring Software Engineer in the New York Metro Area.</p>
+                    
+                    <p className={styles.description}>I'm an aspiring Software Engineer in the New York Metro Area.</p>
                     <p className={styles.contact}>Contact me if you'd like to learn more!</p>
 
                     <div className={styles.buttonContainer}>
                         <a href="mailto:perinuzzic@gmail.com">
-                            <button className={styles.emailButton}>Email</button>
+                            <button className={styles.emailButton}>
+                                <img src="email_img.svg" alt="Email Image" className={styles.icon} />
+                                Email
+                            </button>
                         </a>
 
                         <a href="https://github.com/Claudio-Perinuzzi" target="_blank" rel="noopener noreferrer">
-                            <button className={styles.github}>Github</button>
+                            <button className={styles.github}>
+                                <img src="github-original.svg" alt="GitHub Logo" className={styles.icon} />
+                                Github
+                            </button>
                         </a>
 
                         <a href="https://www.linkedin.com/in/claudio-perinuzzi/" target="_blank" rel="noopener noreferrer">
-                            <button className={styles.linkedin}>LinkedIn</button>
+                            <button className={styles.linkedin}>
+                                <img src="linkedin-plain.svg" alt="LinkedIn Logo" className={styles.icon} />
+                                LinkedIn
+                            </button>
                         </a>
                     </div>
                 </div>
