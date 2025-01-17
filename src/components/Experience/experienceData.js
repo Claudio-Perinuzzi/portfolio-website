e233c864ae3325b7634a49e7b9c4cf5c83ccf249
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-01-16 20:42:57 -0500

    Added experiences

---
+++ src/components/Experience/experienceData.js
@@ -0,0 +1,32 @@
+export const experienceData = [
+    {
+        fileName: 'East_End_Hand_Surgery.txt',
+        title: 'Data Manager',
+        time: 'Present',
+        description: [
+            { text: 'I manage and organize over 2 terabytes of internal records, ensuring data integrity and security of backups.', highlight: false },
+            { text: '', break: true },
+            { text: '', break: true },
+            { text: 'In addition, I develop ', highlight: false },
+            { text: 'internal software tools', highlight: true },
+            { text: ' to streamline and automate tasks such as directory creation and encryption of multiple files, ', highlight: false },
+            { text: 'saving my team over 12 hours each month.', highlight: true },
+        ]
+    },
+    {
+        fileName: 'CUNY_Tech_Prep.txt',
+        title: 'Data Science & Software Engineer Fellow',
+        time: 'Present',
+        description: [
+            { text: 'Accepted into a ', highlight: false },
+            { text: 'highly competitive year-long fellowship ', highlight: true },
+            { text: 'where I utilize machine learning and data science techniques such as ', highlight: false },
+            { text: 'data engineering, data visualization, model evaluation and optimization.', highlight: true },
+            { text: '', break: true },
+            { text: '', break: true },     
+            { text: 'Additionally, I have gained hands-on experience in software engineering practices, including ', highlight: false },
+            { text: 'software design, system architecture, version control, and front-end development with React,', highlight: true }, 
+            { text: 'to create interactive and dynamic user interfaces.', highlight: false }
+        ],
+    },
+];
