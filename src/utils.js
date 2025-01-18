8d958b3cd5b5798a56e54fe3f28b7dfe147210ba
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-01-18 14:20:48 -0500

    Removed utils

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-/* 
-Takes a relative path and appends it to the base url of the website
-Returns the absolute URL of an asset file
-Makes it easier to dynamically import images
-*/
-export const getImageUrl = (relPath) => {
-    return new URL(`/assets/${relPath}`, import.meta.url).href;
-}
