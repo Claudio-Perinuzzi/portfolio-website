a39d3ce17b25bd645c0c21076b427ebc1a0adf17
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-06-19 13:34:55 -0400

    added rendering for posts component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import styles from './App.module.css'
 import { Navbar } from "./components/Navbar/Navbar";
 import { ContactCard } from "./components/ContactCard/ContactCard";
 import { About } from "./components/About/About";
+import { Education } from "./components/Education/Education";
 import { Experience } from "./components/Experience/Experience";
 import { Project } from "./components/Project/Project";
 import { Footer } from "./components/Footer/Footer";
+import { Posts } from "./components/Posts/Posts";
 
 import { experienceData } from './components/Experience/experienceData';
 import { projectData } from './components/Project/projectData';
@@ -15,8 +17,10 @@ function App() {
       <Navbar />
       <ContactCard />
       <About />
+      <Education />
       <Experience  experiences={experienceData}/>
       <Project projects={projectData}/>
+      <Posts />
       <Footer />
     </div>
   )
