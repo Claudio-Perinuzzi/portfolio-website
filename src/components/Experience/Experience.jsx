3e70a2813158435dc13d106b0518d83caa828d62
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-09-28 13:52:41 -0400

    added functions to web links and refactored display of job details

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,20 +1,88 @@
 import React from 'react';
 import styles from './Experience.module.css';
 
+function displayLogo(exp) {
+    if (exp.fileName.includes("Steris.txt")) {
+        return ( 
+            <a href="https://www.steris.com/about/company" target="_blank" rel="noopener noreferrer">
+                <img src={exp.logo} alt="SVG Logo" className={styles.svgLogo} /> 
+            </a>
+        );
+    }
+    else if (exp.fileName.includes("Data_Annotation_Tech.txt")) {
+        return ( 
+            <a href="https://www.dataannotation.tech/" target="_blank" rel="noopener noreferrer">
+                <img src={exp.logo} alt="SVG Logo" className={styles.svgLogo} /> 
+            </a>
+        );
+    }
+    else if (exp.fileName.includes("CUNY_Tech_Prep.txt")) {
+        return ( 
+            <a href="https://cunytechprep.org/" target="_blank" rel="noopener noreferrer">
+                <img src={exp.logo} alt="Default Logo" className={styles.imgContainer} />
+            </a>
+        );
+    }
+    else if (exp.fileName.includes("East_End_Hand_Surgery.txt")) {
+        return ( 
+            <a href="https://www.linkedin.com/company/east-end-hand-surgery/about/" target="_blank" rel="noopener noreferrer">
+                <img src={exp.logo} alt="Default Logo" className={styles.imgContainer} />
+            </a>
+        );
+    }
+    else {
+        return ( <img src={exp.logo} alt="Default Logo" className={styles.imgContainer} /> );    
+    }
+}
+
+function displayText(desc, idx, startOfBullet) { // Creates breaks or highlighted text based on experience data 
+    // If it's a break, output <br> 2x
+    if (desc.break) {
+        startOfBullet.value = true;
+        return (
+            <React.Fragment key={idx}>
+                <br />
+                <br />
+            </React.Fragment>
+        );
+    }
+
+    // Get the text
+    const textContent = (
+        <span key={idx} className={desc.highlight ? styles.highlight : undefined}>
+            {desc.text}
+        </span>
+    );
+
+    // If it's the start of a bullet, output the bullet and text
+    if (startOfBullet.value) {
+        startOfBullet.value = false; // Reset flag
+        return (
+            <React.Fragment key={idx}>
+                <span className={styles.bulletPoint}>{'>> '}</span>
+                {textContent}
+            </React.Fragment>
+        );
+    }
+
+    // Otherwise, just output the text content (continuation of a line)
+    return textContent;    
+}
+
 export const Experience = ({ experiences }) => {
     return (
         <section id="experience">
             <h1 className={styles.experienceHeader}>Experience</h1>
             {experiences.map((exp, index) => {
                 
-                let startOfBullet = true; 
+                let startOfBullet = {value: true}; // Pass by ref
                 
                 return (
                     <div className={styles.windowContainer} key={index}>
                         <div className={styles.tabHeader}>{exp.fileName}</div>
                         <div className={styles.textContainer}>
                             <div className={styles.roleContainer}>
-                                <img src={exp.logo} alt="Logo Image" className={exp.fileName.includes("Steris.txt") ? styles.sterisLogo : styles.imgContainer} />
+                                {displayLogo(exp)}
                                 <div className={styles.textGroup}>
                                     <div className={styles.titleGroup}> 
                                         <span className={styles.role}>Title: </span>
@@ -24,37 +92,9 @@ export const Experience = ({ experiences }) => {
                                 </div>
                             </div>
                             <br />
-                            
-                            {/* Creates breaks or highlighted text based on experience data */}
                             <p className={styles.description}>
                                 {exp.description.map((desc, idx) => {
-                                    
-                                    // If it's a break, output <br> and set flag for next text
-                                    if (desc.break) {
-                                        startOfBullet = true;
-                                        return <br key={idx} />;
-                                    }
-
-                                    // It's text, determine if it needs a bullet
-                                    const textContent = (
-                                        <span key={idx} className={desc.highlight ? styles.highlight : undefined}>
-                                            {desc.text}
-                                        </span>
-                                    );
-
-                                    // If it's the start of a bullet, output the bullet and text
-                                    if (startOfBullet) {
-                                        startOfBullet = false; // Reset flag
-                                        return (
-                                            <React.Fragment key={idx}>
-                                                <span className={styles.bulletPoint}>{'>> '}</span>
-                                                {textContent}
-                                            </React.Fragment>
-                                        );
-                                    }
-
-                                    // Otherwise, just output the text content (continuation of a line)
-                                    return textContent;
+                                    return displayText(desc, idx, startOfBullet)
                                 })}
                             </p>
                         </div>
