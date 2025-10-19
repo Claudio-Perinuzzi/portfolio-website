0ea6c8c0aadeee432636cde42d82154c4b9c2f45
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-10-19 11:41:43 -0400

    added hooks for scroll fade in

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,43 +1,49 @@
 import React from 'react';
 import styles from './Education.module.css';
+import { useScrollFadeIn } from '../../hooks/useScrollFadeIn';
 
 export const Education = () => {
+
+    const { ref: wrapperRef, isVisible } = useScrollFadeIn(0.1); 
+    const wrapperClasses = `${styles.educationWrapper} ${isVisible ? styles.is_visible : ''}`;
+    
     return (
         <>
-            <h1 className={styles.educationHeader}>Education</h1>
-            <section className={styles.windowContainer}>
+            <div ref={wrapperRef} className={wrapperClasses}>
+                <h1 className={styles.educationHeader}>Education</h1>
+                <section className={styles.windowContainer}>
 
-                <div className={styles.tabHeader}>Universities.txt</div>
+                    <div className={styles.tabHeader}>Universities.txt</div>
 
-                <div className={styles.schoolContainer}>
-                    <a href="https://www.qc.cuny.edu/" target="_blank" rel="noopener noreferrer">
-                        <img src="QC_Icon.svg" alt="QC image" className={styles.schoolImg}/>
-                    </a>
-                    
-                    <div className={styles.degreeDetails}> 
-                        <p className={styles.textContainer}>
-                            <span className={styles.masterHighlightColor}>Master's in Computer Science</span>
-                        </p>
+                    <div className={styles.schoolContainer}>
+                        <a href="https://www.qc.cuny.edu/" target="_blank" rel="noopener noreferrer">
+                            <img src="QC_Icon.svg" alt="QC image" className={styles.schoolImg}/>
+                        </a>
                         
-                        <p className={styles.textContainer}>
-                            <span className={styles.bachelorHighlightColor}>Bachelor's in Computer Science</span>
-                        </p>
+                        <div className={styles.degreeDetails}> 
+                            <p className={styles.textContainer}>
+                                <span className={styles.masterHighlightColor}>Master's in Computer Science</span>
+                            </p>
+                            
+                            <p className={styles.textContainer}>
+                                <span className={styles.bachelorHighlightColor}>Bachelor's in Computer Science</span>
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.schoolContainer}>
-                    <a href="https://www.stonybrook.edu/" target="_blank" rel="noopener noreferrer">
-                        <img src="SBU_Icon.svg" alt="SBU image" className={styles.schoolImg}/>
-                    </a>                    
-                <div className={styles.degreeDetails}> 
-                        <p className={styles.textContainer}>
-                            <span className={styles.bachelorHighlightColor}>Bachelor's in Biology</span>
-                        </p>
+                    <div className={styles.schoolContainer}>
+                        <a href="https://www.stonybrook.edu/" target="_blank" rel="noopener noreferrer">
+                            <img src="SBU_Icon.svg" alt="SBU image" className={styles.schoolImg}/>
+                        </a>                    
+                    <div className={styles.degreeDetails}> 
+                            <p className={styles.textContainer}>
+                                <span className={styles.bachelorHighlightColor}>Bachelor's in Biology</span>
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-            </section>
-            
+                </section>
+            </div>
         </>
     );
 };
