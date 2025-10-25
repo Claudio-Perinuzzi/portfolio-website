8acc72815f98b75eb588775e66a9df5e095775e4
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-10-25 18:11:47 -0400

    init achievement jsx

---
+++ src/components/Achievements/Achievements.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import styles from './Achievements.module.css';
+
+export const Achievements = () => {
+
+    // return (
+    //     <section className={styles.portfolioPostsSection} id="interests">
+    //         <h2 className={styles.postsSectionHeader}>Interests</h2>
+
+    //         <p className={styles.interestText}>
+    //             Much of my discipline and personal success stems from my long-standing practice of martial arts.  
+    //             I hold a 2nd-degree rank from Alan Lee's Chinese Kung-Fu Wu-Su Association 
+    //             (<a href="https://kungfuwusu.com/" target="_blank" rel="noopener noreferrer">kungfuwusu.com</a>) and have had the privilege of teaching this rich and powerful art form.  
+    //             Here are a few related posts showcasing my journey and some personal interests of mine:
+    //         </p>
+
+    //         <div className={styles.instagramPostsGrid}>
+    //             {posts.map(post => (
+    //                 <SingleInstagramEmbed // Render the helper component for each post
+    //                     key={post.id}
+    //                     embedHtml={post.embedHtml} 
+    //                 />
+    //             ))}
+    //         </div>
+    //     </section>
+    // );
+};
