90b3fb30a4a1eff33584aa2370203b271617cf8d
Claudio Perinuzzi <perinuzzic@gmail.com>
2025-10-25 18:11:24 -0400

    init achievement data

---
+++ src/components/Achievements/achievementData.js
@@ -0,0 +1,41 @@
+export const achievementData = [
+    {
+        title: 'Academic Achievement Scholarship',
+        time: '2025',
+        imgPath: 'QC_Icon.svg',
+        url: 'https://www.qc.cuny.edu/',
+        description: [
+            { text: 'Recipient of the Samuel and Karola Wachsberger Scholarship for ', highlight: false },
+            { text: 'outstanding academic achievement ', highlight: true },
+            { text: "in the accelerated Master's program.", highlight: false },
+            { text: '', break: true },            
+            { text: '', break: true },
+            { text: "Completed the Master's program in ", highlight: false },
+            { text: "one intensive semester with a 7-course graduate load while simultaneously working part-time (24 hours/week)", highlight: true },
+        ]
+    },     
+    {
+        title: 'Kung-Fu Wu-Su 2nd Degree Instructor',
+        time: '2018 - 2021',
+        imgPath: 'kfwsu_logo.svg',
+        url: 'https://kungfuwusu.com/',
+        description: [
+            { text: 'Temp 1', highlight: false },
+            { text: '', break: true },            
+            { text: '', break: true },
+            { text: 'Temp 2', highlight: true },
+        ]
+    }, 
+    {
+        title: 'Kung-Fu Wu-Su 1st Degree Assistant Instructor',
+        time: '2013 - 2018',
+        imgPath: 'kfwsu_logo.svg',
+        url: 'https://kungfuwusu.com/',
+        description: [
+            { text: 'Temp 1', highlight: false },
+            { text: '', break: true },            
+            { text: '', break: true },
+            { text: 'Temp 2', highlight: true },
+        ]    
+    }
+];
