afafd9f378b403197cb0c57609d8891ab1d91030
Claudio Perinuzzi <perinuzzic@gmail.com>
2026-01-11 11:32:17 -0500

    rewrote to include bash terminal and removed python file

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,29 +6,33 @@ export const About = () => {
         <section id="about">
             <h1 className={styles.aboutHeader}>About</h1>
             <div className={styles.windowContainer}>
-                <div className={styles.tabHeader}>about_me.py</div>
-                    <div className={styles.ideContainer}>
-                        <span className={styles.import}>from</span>{" "}
-                        <span className={styles.variable}>about</span>{" "}
-                        <span className={styles.import}>import</span>{" "}
-                        <span className={styles.function}>me</span> 
-                        <br /> 
-                        <br />
-                        <span className={styles.vaideContainerriable}>myself</span>{" "}
-                        <span className={styles.operator}>=</span>{" "}
-                        <span className={styles.function}>me</span>
-                        <span className={styles.parenthesis}> ()</span>
-                        <br />
-                        <span className={styles.function}>print</span>
-                        <span className={styles.parenthesis}> (</span>
-                        <span className={styles.variable}>myself</span>
-                        <span className={styles.parenthesis}>)</span>
-                    </div>
-                <div className={styles.terminalHeader}>Terminal - zsh</div>
+                {/* Old Legacy Python Terminal Commented out */}
+                {/* <div className={styles.ideContainer}>
+                    <span className={styles.import}>from</span>{" "}
+                    <span className={styles.variable}>about</span>{" "}
+                    <span className={styles.import}>import</span>{" "}
+                    <span className={styles.function}>me</span> 
+                    <br /> 
+                    <br />
+                    <span className={styles.vaideContainerriable}>myself</span>{" "}
+                    <span className={styles.operator}>=</span>{" "}
+                    <span className={styles.function}>me</span>
+                    <span className={styles.parenthesis}> ()</span>
+                    <br />
+                    <span className={styles.function}>print</span>
+                    <span className={styles.parenthesis}> (</span>
+                    <span className={styles.variable}>myself</span>
+                    <span className={styles.parenthesis}>)</span>
+                </div> */}
+                <div className={styles.terminalHeader}>Terminal - bash</div>
                 <div className={styles.terminal}>
-                    last login: {new Date().toLocaleString()}<br /> 
-                    cp ~ % 
-                    <div className={styles.pythonCommand}>python about_me.py</div>
+                    Last login: {new Date().toString()}<br />
+                    <span className={styles.userTerminal}>cp@localhost</span>
+                    :~$
+                    {/* cp ~ %  */}
+                    {/* <div className={styles.pythonCommand}>python about_me.py</div> */}
+                    <div className={styles.bashCommand}>cat about_me.txt</div>
+
                     <br/>
                     <br/>
                     <div className= {styles.aboutText}>
