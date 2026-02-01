1f8b682272bf252585801bc0edf6a2f8c5177276
Claudio Perinuzzi <perinuzzic@gmail.com>
2026-02-01 09:16:36 -0500

    added a card wrapper for including columns

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -14,33 +14,36 @@ export const Education = () => {
                 <section className={styles.windowContainer}>
 
                     <div className={styles.tabHeader}>Universities.txt</div>
-
-                    <div className={styles.schoolContainer}>
+                    
+                    {/* Card wrapper for columns */}
+                    <div className={styles.cardsWrapper}>
                         <a href="https://www.qc.cuny.edu/" target="_blank" rel="noopener noreferrer">
-                            <img src="QC_Icon.svg" alt="QC image" className={styles.schoolImg}/>
-                        </a>
-                        
-                        <div className={styles.degreeDetails}> 
-                            <p className={styles.textContainer}>
-                                <span className={styles.masterHighlightColor}>Master's in Computer Science</span>
-                            </p>
+                            <div className={styles.schoolContainer}>
+                                <img src="QC_Icon.svg" alt="QC image" className={styles.schoolImg}/>
                             
-                            <p className={styles.textContainer}>
-                                <span className={styles.bachelorHighlightColor}>Bachelor's in Computer Science</span>
-                            </p>
-                        </div>
-                    </div>
+                                <div className={styles.degreeDetails}> 
+                                    <p className={styles.textContainer}>
+                                        <span className={styles.masterHighlightColor}>Master's in Computer Science</span>
+                                    </p>
+                                    
+                                    <p className={styles.textContainer}>
+                                        <span className={styles.bachelorHighlightColor}>Bachelor's in Computer Science</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
 
-                    <div className={styles.schoolContainer}>
                         <a href="https://www.stonybrook.edu/" target="_blank" rel="noopener noreferrer">
-                            <img src="SBU_Icon.svg" alt="SBU image" className={styles.schoolImg}/>
-                        </a>                    
-                    <div className={styles.degreeDetails}> 
-                            <p className={styles.textContainer}>
-                                <span className={styles.bachelorHighlightColor}>Bachelor's in Biology</span>
-                            </p>
-                        </div>
-                    </div>
+                            <div className={styles.schoolContainer}>
+                                <img src="SBU_Icon.svg" alt="SBU image" className={styles.schoolImg}/>
+                                <div className={styles.degreeDetails}> 
+                                    <p className={styles.textContainer}>
+                                        <span className={styles.bachelorHighlightColor}>Bachelor's in Biology</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </a> 
+                    </div>                    
 
                 </section>
             </div>
